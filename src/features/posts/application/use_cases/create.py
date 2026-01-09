1d5ae4136193a393471c957a023fe5cc143f17df
BrenDev0 <lahey1991@gmail.com>
2026-01-09 15:09:58 -0600

    auto add publishing date  if postis being published

--- src/features/posts/application/use_cases/create.py
+++ src/features/posts/application/use_cases/create.py
@@ -4,6 +4,7 @@ from src.features.blogs.domain.entities import Blog
 from src.features.posts.domain import entities, schemas
 from src.persistence.domain.exceptions import NotFoundException
 from src.security.domain.exceptions import PermissionsException
+from datetime import datetime, timezone
 
 class CreateBlogPost:
     def __init__(
@@ -32,9 +33,18 @@ class CreateBlogPost:
         if str(blog.user_id) != str(user_id):
             raise PermissionsException()
         
+
+        req_data_to_dict = req_data.model_dump(by_alias=False)
+
+        is_publishing = req_data_to_dict.get("published")
+
+        if is_publishing:
+            publishing_date = datetime.now(timezone.utc)
+            req_data_to_dict["published_at"] = publishing_date
+
         data = entities.BlogPost(
             blog_id=blog_id,
-            **req_data.model_dump(by_alias=False)
+            **req_data_to_dict
         )  
 
         new_post: entities.BlogPost = self.__post_repository.create(
