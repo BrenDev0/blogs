440791d39aab2ff61367238f7f3d4c2559957137
BrenDev0 <lahey1991@gmail.com>
2026-01-20 09:18:19 -0600

    complete

--- src/features/email/interface/strawberry/mutations.py
+++ src/features/email/interface/strawberry/mutations.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 @strawberry.type
 class EmailMutations:
     @strawberry.mutation(
-        description="Send verification code to users email. Will receive a token that can be used for verified requests."
+        description="Send verification code to users email. Will receive a token that can be used for verified requests. This endpoint can also be used with an Auth header to recieve token needed for verifed update to users email"
     )
     @req_validation.validate_input_to_model
     @gather_context.inject_strawberry_context
@@ -62,5 +62,53 @@ class EmailMutations:
         except Exception as e:
             logger.error(str(e))
             raise GraphQlException()
+        
+    
+    @strawberry.mutation(
+        description="Searches for user in db and sends email verification email, token recieved must be used for verified update to users password"
+    )
+    @req_validation.validate_input_to_model
+    def public_account_recovery(
+        self,
+        input: VerifyEmailType
+    ) -> VerificationTokenType:
+        try:
+            use_case = get_verification_email_use_case()
+            rule = get_user_exists_rule()
+        
+            existing_user = rule.validate(email=input.email)
+
+            code = get_random_code(len=6)
+
+            use_case.execute(to=input.email, verification_code=code)
+
+            web_token_service = get_web_token_service()
+        
+            token_payload = {
+                "user_id": existing_user.user_id,
+                "verification_code": code
+            }
+
+            verification_token = web_token_service.generate(
+                payload=token_payload,
+                expiration=900
+            )
+
+            return VerificationTokenType(
+                verification_token=verification_token
+            )
+        
+        except NotFoundException as e:
+            raise GraphQlException(str(e))
+        
+        except Exception as e:
+            logger.error(str(e))
+            raise GraphQlException()
+
+
+
+
+
+            
         
     
